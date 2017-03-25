35fa1b7db09def46b94cf31d2112b96ffbab7c4c
Vincent Yan <vincentyan1@gmail.com>
2017-03-24 21:20:46 -0700

    Added error handling, style to list items

--- js/app.js
+++ js/app.js
@@ -1,22 +1,46 @@
 $(document).ready(function(){
+	// performs search when Enter key is pressed
 	$(".search-box").submit(searching);
+	// performs search when Search icon is clicked
 	$(".search-img").click(searching);
+	// opens up new window and displays random Wikipedia page
+	$(".random").click(function(){
+		window.open("https://en.wikipedia.org/wiki/Special:Random");
+	})
 });
 
 function searching(event){
 	event.preventDefault();
+	// user input is stored onto a variable
 	var userInput = $(".input").val();
+	// this is the wikipedia API endpoint
 	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + userInput + "&format=json&callback?";
 	var $results = $(".result-items");
+	// clears search results when a new search is performed
+	$results.html('');
 
+	moveUp();
+	
 	$.ajax({
 		type: "GET",
 		dataType: "jsonp",
 		url: wikiUrl,
 		success: function(data){
+			// iterates through each list item and appends wikipedia data from
+			// user input
 			for (var i = 0; i < data[1].length; i++) {
-				$results.append("<li class='item'><h2 class='item-header'><a href='" + data[3][i] + "'>" + data[1][i] + "</a></h2><p class='summary'>" + data[2][i] +"</p></li>");
+				$results.append("<a class='item-link' href='" + data[3][i] + "' target='_blank'><li class='list-item'><h2 class='item-header'>" + data[1][i] + "</h2><p class='summary'>" + data[2][i] +"</p></li></a>");
 			}
+		},
+		error: function(){
+			$results.append("<li class='list-item'><h2 class='item-header'>Error! Could not retrieve data</h2></li>")
 		}
-	})
+	});
+}
+
+// this animate content div to make way for search results
+function moveUp(){
+	$(".content").animate({
+		margin: "0",
+	}, 300);
 }
